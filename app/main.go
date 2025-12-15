5ad119d329508c649df4d74194b6d3439fb67e86
Viacheslav Poturaev <vearutop@gmail.com>
2025-12-15 16:56:48 +0100

    Add transposed results

--- app/main.go
+++ app/main.go
@@ -315,6 +315,12 @@ func Main() error { //nolint:funlen,cyclop,maintidx
 		}
 	})
 	dbcon.Mount(s, "/", dbcon.DefaultDeps(instances), func(options *dbcon.Options) {
+		options.Completions = append(options.Completions, dbcon.SQLCompletion{
+			Value: "-- transpose",
+			Score: 1000,
+			Meta:  "transpose result",
+		})
+
 		options.AddValueProcessor("img", func(v any) any {
 			if b, ok := v.([]byte); ok {
 				ct := http.DetectContentType(b)
